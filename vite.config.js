910f98a6839120515c1a3df61d0a236c2cb10d3a
auwaisqureshi <auwaisqureshi@gmail.com>
2026-03-11 16:32:51 +0530

    chore: bump MUI to latest and add resolve extensions

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    // include .js extension to help Rollup find files in dependencies
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
+  },
   plugins: [react(), svgr()],
 })
